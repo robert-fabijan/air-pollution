c5d255c7d5a5ac5cf0b5908cfb5d60d2bec005b5
robert-fabijan <robert.f.fabijan@gmail.com>
2024-01-26 16:23:57 +0000

    Added Terraform Script

--- tf/main.tf
+++ tf/main.tf
@@ -1,11 +1,34 @@
-provider "google" {
-  credentials = file("C:\\Users\\matacza\\Desktop\\Projekty\\DE\\Pobieranie Danych (API)\\code\\secrets\\google_cloud_service_key.json")
-  project     = "phonic-vortex-398110"
-  region      = "us-central1"
+
+
+# Set up Cloud Function for air-pollution API data extraction
+resource "google_cloudfunctions_function" "air-pollution-api-function" {
+  name                  = "air-pollution-api-function"
+  description           = "Function to retrieve data from Openweather API"
+  runtime               = "python11"
+  available_memory_mb   = 128
+  source_repository {
+    url = "https://source.developers.google.com/projects/${var.gcp_project}/repos/${var.repository_name}/moveable-aliases/${var.branch_name}/paths/${var.source_directory}"
+  }
+  trigger_http          = true
+  entry_point           = "gcloud_get_openweather_data_function"
 }
 
-resource "google_storage_bucket" "amatacz_air_pollution_bucket" {
-  name     = "air_pollution_bucket_amatacz"
-  location = "EU"
 
-}
+# Create Pub/Sub topic 
+# resource "google_pubsub_topic" "basic_topic" {
+#   name = "example-topic"
+# }
+
+# resource "google_pubsub_subscription" "basic_subscription" {
+#   name  = "example-subscription"
+#   topic = google_pubsub_topic.basic_topic.name
+# }
+
+
+# # Create Workflow to manage data flow from Cloud Function to Pub/Sub
+# resource "google_workflows_workflow" "basic_workflow" {
+#   name    = "example-workflow"
+#   region  = "us-central1"
+
+#   source_contents = file("${path.module}/path/to/your/workflow.yaml")
+# }
---
+++ tf/terraform.tfvars
@@ -0,0 +1,3 @@
+gcp_svc_key = "..\gcloud\configs\credentials-terraform.json"
+gcp_project = "terraform-test-project-412415"
+gcp_region = "europe-west2"
